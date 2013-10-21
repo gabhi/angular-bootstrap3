6771ef42c62972c54d5cc6d9e02bde696c85fe08
Abhijit Gaikwad <agaikwad@marketlive.com>
2013-10-21 01:20:58 -0700

    no message

--- js/controllers.js
+++ js/controllers.js
@@ -6,10 +6,49 @@ angular.module('myApp.controllers', []).
 controller('MyCtrl1', [function() {
 
 }])
-.controller('QstnCtrl', ['$scope', '$routeParams',
+.controller('QstnsCtrl', ['$scope', '$routeParams',
 	function($scope, $routeParams) {
 		$scope.typeId = $routeParams.typeId;
 		$scope.catId = $routeParams.catId;
+
+
+		var values1 = [
+		{name:'Arrays', age:25, gender:'boy'},
+		{name:'Matrix', age:30, gender:'girl'},
+		{name:'Linked List', age:28, gender:'girl'},
+		{name:'Stacks & Ques', age:15, gender:'girl'},
+		{name:'Trees & Graphs', age:28, gender:'girl'},
+		{name:'Searching', age:95, gender:'boy'},
+		{name:'Sorting', age:50, gender:'boy'} 
+		];
+		var values2 = [
+		{name:'Arrays', age:25, gender:'boy'},
+		{name:'Matrix', age:30, gender:'girl'},
+		{name:'Linked List', age:28, gender:'girl'},
+		{name:'Stacks & Ques', age:15, gender:'girl'},
+		{name:'Trees & Graphs', age:28, gender:'girl'},
+		{name:'Searching', age:95, gender:'boy'},
+		{name:'Sorting', age:50, gender:'boy'} 
+		];
+
+  //1. function way
+  $scope.getValues = function(typeId,catId) {
+  	if(typeId === 'basic') {
+  		return values1;
+  	}
+  	 
+  }
+
+
+
+}
+
+
+])
+.controller('QstnCtrl', ['$scope', '$routeParams',
+	function($scope, $routeParams) {
+		$scope.qstnId = $routeParams.qstnId;
+		 
 
 
 		var values1 = [
