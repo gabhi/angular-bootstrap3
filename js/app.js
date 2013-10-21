50a60e23b6fcb288e91a06855fa77d0907db39cd
Abhijit Gaikwad <agaikwad@marketlive.com>
2013-10-21 00:07:28 -0700

    no message

--- js/app.js
+++ js/app.js
@@ -13,5 +13,9 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/basic', {templateUrl: 'partials/basic.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/intermediate', {templateUrl: 'partials/intermediate.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/advanced', {templateUrl: 'partials/advanced.html', controller: 'MyCtrl1'});
+
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
